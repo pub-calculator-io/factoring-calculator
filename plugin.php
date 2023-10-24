feee559879c30808ae37cfb5c815e2a2e8cc1330
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:01:02 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_factoring_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Factoring Calculator by Calculator.iO";
 
 function display_ci_factoring_calculator(){
